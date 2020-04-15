314d4fe333d666a2bd79aa6665506aa18966f79d
Renato Pozzi <renato.pozzi.dev@gmail.com>
2020-04-15 18:56:13 +0200

    Refactoring routing module \w strong express routing.

---
+++ initRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const getConfiguration = require("./utils/getConfiguration");
+const runPipeline = require("./utils/runPipeline");
+
+module.exports = initRoutes = () => {
+  let router = express.Router();
+
+  // Managed by configuration YAML
+  const config = getConfiguration();
+
+  if (!config) return res.status(500).send("Error during read configuration.");
+
+  for (const name of Object.keys(config.endpoints)) {
+    const endpoint = config.endpoints[name];
+
+    endpoint.methods.forEach((method) => {
+      endpoint.paths.forEach((path) => {
+        // Get the pipeline which contains endpoint
+        const pipeline = Object.keys(config.pipelines).find((pipelineName) => {
+          return config.pipelines[pipelineName].endpoints.indexOf(name) !== -1;
+        });
+
+        if (!pipeline)
+          console.log(`Warning: Cannot find pipeline for endpoint: ${name}`);
+
+        // Push routes
+        router[method.toLowerCase()](path, (req, res) =>
+          runPipeline(req, res, pipeline)
+        );
+      });
+    });
+  }
+
+  return router;
+};
