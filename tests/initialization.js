89d5750f917f2f5c9488884a0da8be7bcb0cfb01
Renato Pozzi <renato.pozzi.dev@gmail.com>
2020-04-22 14:03:30 +0200

    Added simple test configuration coverage.

---
+++ tests/initialization.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+
+describe("Load Freschissimo and check example config", function () {
+  var server;
+
+  beforeEach(function () {
+    server = require("../index");
+  });
+
+  it("responds to /freschissimo", function testConfiguration(done) {
+    request(server).get("/freschissimo").expect(200, done);
+  });
+});
