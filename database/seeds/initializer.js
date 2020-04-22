2e8120df101d1a9b06041bb9768259eb6a0aa4f4
Renato Pozzi <renato.pozzi.dev@gmail.com>
2020-04-22 17:07:23 +0200

    Solved Codebeat

--- database/seeds/initializer.js
+++ database/seeds/initializer.js
@@ -16,27 +16,27 @@ mongoose
     process.exit();
   });
 
-async function makeUser() {
-  const payload = {
-    firstname: "Freschissimo",
-    lastname: "Gateway",
-    username: "freschissimo",
-    email: "freschissimo@example.com",
-    credentials: [
-      {
-        type: "basicAuth",
-        password: bcrypt.hashSync("password", 10),
-        id: "b2edb4b2-7759-46bc-ab57-4416cc4b0efc",
-      },
-      {
-        type: "jwt",
-        secret: "52dd5bd9-5995-4b60-8678-8c427c25dfa9",
-        scopes: [],
-        id: "31ec163f-c9c1-4887-889d-aa5dfbeb5768",
-      },
-    ],
-  };
+const payload = {
+  firstname: "Freschissimo",
+  lastname: "Gateway",
+  username: "freschissimo",
+  email: "freschissimo@example.com",
+  credentials: [
+    {
+      type: "basicAuth",
+      password: bcrypt.hashSync("password", 10),
+      id: "b2edb4b2-7759-46bc-ab57-4416cc4b0efc",
+    },
+    {
+      type: "jwt",
+      secret: "52dd5bd9-5995-4b60-8678-8c427c25dfa9",
+      scopes: [],
+      id: "31ec163f-c9c1-4887-889d-aa5dfbeb5768",
+    },
+  ],
+};
 
+async function makeUser() {
   var user = await User.create(payload).catch((err) =>
     console.log("An error has occurred", err)
   );
